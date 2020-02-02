6557959d640dcd294df00f804deeafd6f5e45472
fr0nk3nst33n <josh@masterminddesign.io>
2020-02-02 16:03:40 -0700

    Added call to API and added Loading Component

--- components/Work/Work.js
+++ components/Work/Work.js
@@ -1,116 +1,116 @@
+import React, { Component } from "react";
 import Link from "next/link";
+import Loading from "../Loading/Loading";
+import { API_IP, DEFAULT_DATA } from "../../utils/api";
 
 import "./Work.scss";
 
-const Work = () => {
-    return (
-        <div className="work">
-            <h1>Work</h1>
-            <div className="projects">
-                <div className="row">
-                    <div className="col-md-12">
-                        <a
-                            href="https://github.com/MastermindDesign/masterminddesign"
-                            target="_blank"
-                            aria-label="Mastermind Design"
-                        >
-                            <div className="card">
-                                <div className="card-body">
-                                    <div className="card-title">
-                                        <h4>Mastermind Design</h4>
-                                    </div>
-                                    <div className="card-text">
-                                        <span>
-                                            <i className="fas fa-code"></i>
-                                            React, SCSS, Webpack,
-                                            2077-Components
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-12">
-                        <a
-                            href="https://github.com/MastermindDesign/masterminddesign-api"
-                            target="_blank"
-                            aria-label="Mastermind Design API"
-                        >
-                            <div className="card">
-                                <div className="card-body">
-                                    <div className="card-title">
-                                        <h4>Mastermind Design API</h4>
-                                    </div>
-                                    <div className="card-text">
-                                        <span>
-                                            <i className="fas fa-code"></i>
-                                            GO
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-12">
-                        <a
-                            href="https://github.com/MastermindDesign/2077-components-react"
-                            target="_blank"
-                            aria-label="2077-Components-React"
-                        >
-                            <div className="card">
-                                <div className="card-body">
-                                    <div className="card-title">
-                                        <h4>2077-Components-React</h4>
-                                    </div>
-                                    <div className="card-text">
-                                        <span>
-                                            <i className="fas fa-code"></i>
-                                            React, SCSS, Webpack, Bootstrap
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-12">
-                        <Link href="/fithub" aria-label="FitHub">
-                            <div className="card">
-                                <div className="card-body">
-                                    <div className="card-title">
-                                        <h4>FitHub</h4>
-                                    </div>
-                                    <div className="card-text">
-                                        <span>
-                                            <i className="fas fa-code"></i>
-                                            .Net Core, EFCore, MySQL, MongoDB,
-                                            React, SCSS, Webpack
-                                        </span>
+class Work extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true,
+            work: [],
+            seconds: 0
+        };
+    }
+
+    tick = () => {
+        if (this.state.seconds < 100)
+            this.setState(state => ({
+                seconds: state.seconds + 1
+            }));
+    };
+
+    componentDidMount() {
+        this.interval = setInterval(() => this.tick(), 100);
+        setTimeout(() => {
+            fetch(`http://${API_IP}/api/projects`)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    this.setState({
+                        loading: false,
+                        seconds: 100,
+                        work: [...data]
+                    });
+                })
+                .catch(e => {
+                    console.error("Request for Projects Failed!", e);
+                    this.setState({
+                        loading: false,
+                        seconds: 100,
+                        work: [...DEFAULT_DATA]
+                    });
+                });
+        }, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render() {
+        const { loading, seconds, work } = this.state;
+        return (
+            <div className="work">
+                <h1>Work</h1>
+                {!loading ? (
+                    <div className="projects">
+                        {work.map((project, index) => {
+                            return (
+                                <div className="row" key={project._id}>
+                                    <div className="col-md-12">
+                                        <Link href={project.link}>
+                                            <a
+                                                href={project.link}
+                                                target={
+                                                    index != work.length - 1
+                                                        ? "_blank"
+                                                        : ""
+                                                }
+                                                aria-label={project.name}
+                                            >
+                                                <div className="card">
+                                                    <div className="card-body">
+                                                        <div className="card-title">
+                                                            <h4>
+                                                                {project.name}
+                                                            </h4>
+                                                        </div>
+                                                        <div className="card-text">
+                                                            <span>
+                                                                <i className="fas fa-code"></i>
+                                                                {project.stack}
+                                                            </span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </Link>
                                     </div>
                                 </div>
-                            </div>
-                        </Link>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-12">
-                        <div className="card card-small">
-                            <div className="card-body">
-                                <div className="card-text">
-                                    <h4>More to come...</h4>
+                            );
+                        })}
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div className="card card-small">
+                                    <div className="card-body">
+                                        <div className="card-text">
+                                            <h4>More to come...</h4>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                ) : (
+                    <Loading progress={seconds} />
+                )}
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default Work;
---
+++ utils/api.js
@@ -0,0 +1,30 @@
+export const API_IP = "142.93.13.189";
+export const DEFAULT_DATA = [
+    {
+        _id: "5e2e126ce7179a2f0113a0d1",
+        name: "Mastermind Design",
+        stack: "React, Webpack, SCSS, 2077 Design System",
+        link: "https://github.com/MastermindDesign/mastermind-nextjs"
+    },
+    {
+        _id: "5e2e1379e7179a2f0113a0ee",
+        name: "Mastermind Design API",
+        stack: "Go, MongoDB",
+        link: "https://github.com/MastermindDesign/masterminddesign-api",
+        order: 1
+    },
+    {
+        _id: "5e2e1407e7179a2f0113a0f7",
+        name: "2077 Design System",
+        stack: "React, SCSS, Webpack, Lerna, Tailwind",
+        link: "https://github.com/MastermindDesign/2077-components-react",
+        order: 2
+    },
+    {
+        _id: "5e2e146ee7179a2f0113a0fb",
+        name: "FitHub",
+        stack: "Rails, Postgres",
+        link: "fithub",
+        order: 3
+    }
+];
